82f57e0e85a844ac5b8764d6bcdba7a5fa8ba99a
VenkateswaraReddy <50886280+Venky9630@users.noreply.github.com>
2022-01-20 20:45:42 +0530

    Delete BookingFlight-Service directory

--- BookingFlight-Service/src/main/java/com/flightapp/BookingFlightService/Service/BookingFlightService.java
+++
@@ -1,9 +0,0 @@
-package com.flightapp.BookingFlightService.Service;
-
-import com.flightapp.BookingFlightService.dto.BookingFlightDto;
-import com.flightapp.BookingFlightService.ui.BookingFlightResponseModel;
-
-public interface BookingFlightService {
-
-	public BookingFlightResponseModel registerAirline(BookingFlightDto airlineDto);
-}
